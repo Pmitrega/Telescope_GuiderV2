2b855c5fd7b2f6c29ce509c72a210d773106af77
Pmitrega <pawel.mitrega1@wp.pl>
2025-08-25 22:14:00 +0200

    inutual star detection

--- starProcessor/main.cpp
+++ starProcessor/main.cpp
@@ -70,9 +70,48 @@ int main(){
                     img.convertTo(img, CV_16UC1, 256.0);
                     break;
             }
-            double mean = cv::mean(img)[0];
+            cv::Scalar im_mean, im_std;
+            cv::meanStdDev(img, im_mean, im_std);
             auto stop =  std::chrono::high_resolution_clock::now();
-            std::cout << "New image recieved ... Mean brightness: "<< mean <<  " Exec time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << std::endl;
+            if(im_mean[0] < 15000.0 && im_std[0] < 2000.0){
+                cv::Mat img_filt;
+                cv::GaussianBlur(img, img_filt, cv::Size(15, 15), 6);
+                cv::subtract(img, img_filt, img_filt);
+                cv::medianBlur(img_filt, img_filt, 3);
+                cv::Mat binary_im;
+                cv::threshold(img_filt, binary_im, 4000, 65536,cv::THRESH_BINARY);
+                cv::Mat binary_8u;
+                binary_im.convertTo(binary_8u, CV_8U, 255.0/65535.0);
+                cv::Mat labels, stats, centroids;
+                int num_objects = cv::connectedComponentsWithStats(
+                    binary_8u,
+                    labels,
+                    stats,
+                    centroids,
+                    8,                // connectivity: 4 or 8
+                    CV_32S            // label type
+                );
+                    // Loop through each object (skip label 0 = background)
+                for (int i = 1; i < num_objects; i++) {
+                    int x = stats.at<int>(i, cv::CC_STAT_LEFT);
+                    int y = stats.at<int>(i, cv::CC_STAT_TOP);
+                    int w = stats.at<int>(i, cv::CC_STAT_WIDTH);
+                    int h = stats.at<int>(i, cv::CC_STAT_HEIGHT);
+                    int area = stats.at<int>(i, cv::CC_STAT_AREA);
+
+                    double cx = centroids.at<double>(i, 0);
+                    double cy = centroids.at<double>(i, 1);
+
+                    std::cout << "Object " << i << ": area=" << area 
+                            << ", bbox=(" << x << "," << y << "," << w << "," << h << ")"
+                            << ", center=(" << cx << "," << cy << ")" << std::endl;
+                }
+
+                // cv::imwrite("img.png", binary_im);
+                // cv::imwrite("img_org.png", img);
+                // cv::imwrite("img_filt.png", img_filt);
+            }
+            std::cout << "New image recieved ... Mean brightness: "<< im_mean[0] << "std:" << im_std[0] <<  " Exec time: " << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << std::endl;
             last_ID = img_info->ID;
         }
 
